e9eb0139396f0b509ab5421acf62087b19c2910c
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-11-15 14:57:43 +0200

    going

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Облік співробітників у компанії "Jucca" </h1>
+            <h2>Загальна кількість співробітників: </h2>
+            <h2>Пермію отримають: </h2>
+     </div>
+    );
+}
+
+export default AppInfo;
---
+++ src/components/app/app.js
@@ -0,0 +1,22 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from "../app-filter/app-filter";
+import EmployersListItem from '../employers-list-item/employers-list-item';
+import './app.css';
+
+
+function App() {
+    return (
+      <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+                <SearchPanel />
+                <AppFilter />
+                <EmployersListItem/>
+        </div>
+      </div>
+    );
+}
+
+export default  App;
