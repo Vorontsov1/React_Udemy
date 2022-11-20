ef4b704ec7fb7e9afb9af686025e109c308b1fe9
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-11-20 22:53:59 +0200

    add_props

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -3,7 +3,7 @@ import './app-info.css';
 const AppInfo = () => {
     return (
         <div className="app-info">
-            <h1>Облік співробітників у компанії "Jucca" </h1>
+            <h1>Облік співробітників у компанії "Metallica" </h1>
             <h2>Загальна кількість співробітників: </h2>
             <h2>Премію отримають: </h2>
      </div>
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,27 +1,32 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
-import AppFilter from "../app-filter/app-filter";
+import AppFilter from '../app-filter/app-filter';
 import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
-
 function App() {
-    return (
-      <div className="app">
-        <AppInfo />
 
-        <div className="search-panel">
-                <SearchPanel />
-                <AppFilter />
-                
-            </div>
-            
-            <EmployersList />
-            <EmployersAddForm/>
-            
+  const data = [
+    { name: 'James Hetfield', salary: 5000, increase: true },
+    { name: 'Lars Ulrich', salary: 3800, increase: false },
+    { name: 'Kirk Hammet', salary: 3760, increase: false },
+    { name: 'Robert Trujillo', salary: 3940, increase: false },
+  ];
+
+  return (
+    <div className="app">
+      <AppInfo />
+
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
       </div>
-    );
+
+      <EmployersList data={data} />
+      <EmployersAddForm />
+    </div>
+  );
 }
 
-export default  App;
+export default App;
