f360148cba8e907b9fd236546dc8640709eee221
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2022-12-05 11:30:37 +0200

    135_lesson

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -5,15 +7,29 @@ import EmployersList from '../employers-list/employers-list';
 import EmployersAddForm from '../employers-add-form/employers-add-form';
 import './app.css';
 
-function App() {
+class  App extends Component {
+    constructor(props) {
+      super(props);
+        this.state = {
+          data:  [
+            { name: 'James Hetfield', salary: 5000, increase: true, id: 1},
+            { name: 'Lars Ulrich', salary: 3800, increase: false, id: 2},
+            { name: 'Kirk Hammet', salary: 3760, increase: false, id: 3},
+            { name: 'Robert Trujillo', salary: 3940, increase: false, id: 4},
+          ]
+        }
+    }
+  
 
-  const data = [
-    { name: 'James Hetfield', salary: 5000, increase: true, id: 1},
-    { name: 'Lars Ulrich', salary: 3800, increase: false, id: 2},
-    { name: 'Kirk Hammet', salary: 3760, increase: false, id: 3},
-    { name: 'Robert Trujillo', salary: 3940, increase: false, id: 4},
-  ];
+    deleteItem = (id) => {
+      this.setState (({data}) => {
+        return {
+          data: data.filter(item => item.id !== id)
+        } 
+      })
+    }
 
+ render() {
   return (
     <div className="app">
       <AppInfo />
@@ -23,10 +39,12 @@ function App() {
         <AppFilter />
       </div>
 
-      <EmployersList data={data} />
+      <EmployersList data={this.state.data} 
+      onDelete={this.deleteItem}/>
       <EmployersAddForm />
     </div>
   );
+ }
 }
 
 export default App;
--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -5,7 +5,8 @@ class EmployersListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      increase: false
+      increase: false,
+      rise: false
     }
   }
 onIncrease = () => {
@@ -14,9 +15,15 @@ onIncrease = () => {
   }))
 }
 
+onRise = () => {
+  this.setState(({rise}) => ({
+    rise: !rise
+  }))
+}
+
   render() {
 
-    const {name, salary} = this.props;
+    const {name, salary, onDelete} = this.props;
 
     const {increase} = this.state;
 
@@ -40,7 +47,8 @@ onIncrease = () => {
             <i className="fas fa-cookie"></i>
           </button>
 
-          <button type="button" className="btn-trash btn-sm ">
+          <button type="button" className="btn-trash btn-sm "
+          onClick={onDelete}>
             <i className="fas fa-trash"></i>
           </button>
           <i className="fas fa-star"></i>
